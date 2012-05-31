79660057803227f41ffb53a76d11f2fa0d3af7f4
Li Yong <kaven276@vip.sina.com>
2012-05-31 09:25:26 +0800

    total save

---
+++ SGIP.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: kaven276
+ * Date: 12-5-29
+ * Time: 上午8:39
+ */
+
+var mCmd = require('./lib/Commands.js');
+exports.Cmds = mCmd.Cmds;
+exports.nodeSP = require('./lib/nodes/nodeSP.js');
+exports.nodeSMG = require('./lib/nodes/nodeSMG.js');
+exports.nodeSMSC = require('./lib/nodes/nodeSMSC.js');
+exports.nodeGNS = require('./lib/nodes/nodeGNS.js');
---
+++ test/test1.js
@@ -0,0 +1,6 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: kaven276
+ * Date: 12-5-29
+ * Time: 上午8:36
+ */
